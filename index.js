d2f6aea72c194a8733d764fccf8c395137a532f2
tyk2013 <ken.rad0202@gmail.com>
2016-05-14 16:32:41 +0900

    create_system_added

--- index.js
+++ index.js
@@ -24,10 +24,26 @@ var Schema = mongoose.Schema;
 var users = mongoose.Schema({
     name: String
 });
+var goals = mongoose.Schema({
+    top_right: String,
+    top_straight:String,
+    top_left: String,
+    middle_right: String,
+    middle_straight: String,
+    middle_left: String,
+    under_right: String,
+    under_straight: String,
+    under_left: String
+})
 //データベースの一般的なモデルを"User"として命名
 mongoose.model('User', users);
+mongoose.model("Goal", goals);
 //モデルの型を取得
 var User = mongoose.model("User");
+var Goal = mongoose.model("Goal");
+var goallists = [];
+var userlists = [];
+
 ////型からインスタンス取得
 //var user = new User();
 //user.name = "kenzo";
@@ -36,16 +52,7 @@ var User = mongoose.model("User");
 //    console.log("データ挿入成功");
 //})
 
-
-
-////fs.readFileSync(file[, options])はファイル読み込みのみ。readFileの同期ver
-//var content = {
-//    title: "目標決める君",
-//    count: 0
-//}
-
 app.get("/", function (req, res) {
-    var userlists = [];
     //mongodbに検索かける
     User.find({}, function (err, users) {
         if (!err) {
@@ -53,6 +60,7 @@ app.get("/", function (req, res) {
             for (var i = 0; i < users.length; i++) {
                 userlists.push(users[i]);
             }
+            
             res.render(index_filename, {
                 title: "目標決める君",
                 userlists: userlists
@@ -68,8 +76,52 @@ app.get("/404", function (req, res) {
 })
 
 app.post("/", function (req, res) {
-    res.render(main_filename, {
-        name: req.body.name
+    goallists =[];
+    Goal.find({},function(err,goals){
+        if(err) throw err
+        for (var i = 0; i<goals.length; i++){
+            goallists.push(goals[i]);
+        }
+        res.render(main_filename, {
+        name: req.body.name,
+        goallists:goallists
+    });
+    })
+});
+
+app.post("/main" ,function(req,res){
+    var goal = new Goal({
+        top_right:req.body.top_right,
+        top_straight:req.body.top_straight,
+        top_left:req.body.top_left,
+        middle_right:req.body.middle_right,
+        middle_straight:req.body.middle_straight,
+        middle_left:req.body.middle_left,
+        under_right:req.body.under_right,
+        under_straight:req.body.under_straight,
+        under_left:req.body.under_left,
+    });
+    goal.save(function(err){
+        if(err){
+            console.log("保存できてないよ！");
+        }else{
+            console.log("保存できました！")
+        }
+    });
+    res.redirect("/main")
+})
+
+app.get("/main",function(req,res){
+    goallists =[];
+    Goal.find({},function(err,goals){
+        if(err) throw err
+        for (var i = 0; i<goals.length; i++){
+            goallists.push(goals[i]);
+        }
+        res.render(main_filename,{
+            name:"岡野",
+            goallists:goallists
+    });
     })
 })
 
