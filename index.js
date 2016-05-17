1a854b7381d7d0fceaa48121047f7d33d8a0ae8a
tyk2013 <ken.rad0202@gmail.com>
2016-05-17 16:49:20 +0900

    認証機能追加

--- index.js
+++ index.js
@@ -3,17 +3,26 @@ var fs = require("fs");
 var express = require("express");
 var bodyParser = require("body-parser");
 var method_override = require("method-override");
+var express_helpers = require('express-helpers')();
 var app = express();
+
+var passport = require('passport')
+    , LocalStrategy = require('passport-local').Strategy;
 //viewレンダリングエンジンをejsに設定
 app.set('view engine', 'ejs');
-app.use( method_override('_method') );
+app.use(method_override('_method'));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(passport.initialize());
 
+app.use(express.static(__dirname + '/public'));
 //ファイルへのパス
-var index_filename = __dirname + "/template/index.ejs";
-var main_filename = __dirname + "/template/main.ejs";
+var index_filename = __dirname + "/public/template/index.ejs";
+var main_filename = __dirname + "/public/template/main.ejs";
+var subscribe_filename = __dirname + "/public/template/subscribe.ejs";
+var message_filename = __dirname + "/public/template/message.ejs";
+
 
 //データベース設定
 var mongodb = require("mongodb");
@@ -23,20 +32,62 @@ mongoose.connect('mongodb://localhost/test');
 var Schema = mongoose.Schema;
 //データベースの中身を具体的にカラムごと設定
 var users = mongoose.Schema({
-    name: String
+    username: {
+        type: String, required: true, index: {unique: true}
+    }
+    , password: {
+        type: String
+        , required: true
+    }
 });
+
+users.methods.validPassword = function (pwd) {
+    // EXAMPLE CODE!
+    return (this.password === pwd);
+};
+//トップページの認証設定
+passport.use(new LocalStrategy(
+    function (username, password, done) {
+        User.findOne({
+            username: username
+        }, function (err, user) {
+            if (err) {
+                return done(err);
+            }
+            if (!user) {
+                return done(null, false);
+            }
+            if (!user.validPassword(password)) {
+                return done(null, false);
+            }
+            return done(null, user);
+        });
+    }
+));
+//<-----パスワードをシリアル化 
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
+// -------------->
+
+
 var goals = mongoose.Schema({
-    top_right: String,
-    top_straight:String,
-    top_left: String,
-    middle_right: String,
-    middle_straight: String,
-    middle_left: String,
-    under_right: String,
-    under_straight: String,
-    under_left: String
-})
-//データベースの一般的なモデルを"User"として命名
+        top_right: String
+        , top_straight: String
+        , top_left: String
+        , middle_right: String
+        , middle_straight: String
+        , middle_left: String
+        , under_right: String
+        , under_straight: String
+        , under_left: String
+    })
+//データベースのモデルを"User"、"Goal"として命名
 mongoose.model('User', users);
 mongoose.model("Goal", goals);
 //モデルの型を取得
@@ -44,27 +95,26 @@ var User = mongoose.model("User");
 var Goal = mongoose.model("Goal");
 var goallists = [];
 var userlists = [];
+// ルーディング設定
 
-////型からインスタンス取得
-//var user = new User();
-//user.name = "kenzo";
-//user.save(function (err) {
-//    if (err) throw err;
-//    console.log("データ挿入成功");
-//})
+
+//ログイン機能
+app.post('/login', passport.authenticate('local', {
+        failureRedirect: '/404',
+        successRedirect: '/goals'
+    }));
 
 app.get("/", function (req, res) {
     //mongodbに検索かける
     User.find({}, function (err, users) {
         if (!err) {
-            console.log("num of item => " + users.length)
             for (var i = 0; i < users.length; i++) {
                 userlists.push(users[i]);
             }
-            
+
             res.render(index_filename, {
-                title: "目標決める君",
-                userlists: userlists
+                title: "目標決める君"
+                , userlists: userlists
             });
         } else {
             console.log("find error")
@@ -77,62 +127,80 @@ app.get("/404", function (req, res) {
 })
 
 app.post("/", function (req, res) {
-    goallists =[];
-    Goal.find({},function(err,goals){
-        if(err) throw err
-        for (var i = 0; i<goals.length; i++){
+    goallists = [];
+    Goal.find({}, function (err, goals) {
+        if (err) throw err
+        for (var i = 0; i < goals.length; i++) {
             goallists.push(goals[i]);
         }
         res.render(main_filename, {
-        name: req.body.name,
-        goallists:goallists
-    });
+            name: req.body.name
+            , goallists: goallists
+        });
     })
 });
 
-app.post("/goals" ,function(req,res){
+app.get("/goals", function (req, res) {
+    console.log(req)
+    goallists = [];
+    Goal.find({}, function (err, goals) {
+        if (err) throw err
+        for (var i = 0; i < goals.length; i++) {
+            goallists.push(goals[i]);
+        }
+        res.render(main_filename, {
+            username: req.body.username
+            , goallists: goallists
+        });
+    })
+});
+
+app.post("/goals", function (req, res) {
     var goal = new Goal({
-        top_right:req.body.top_right,
-        top_straight:req.body.top_straight,
-        top_left:req.body.top_left,
-        middle_right:req.body.middle_right,
-        middle_straight:req.body.middle_straight,
-        middle_left:req.body.middle_left,
-        under_right:req.body.under_right,
-        under_straight:req.body.under_straight,
-        under_left:req.body.under_left,
-    });
-    goal.save(function(err){
-        if(err){
+        top_right: req.body.top_right
+        , top_straight: req.body.top_straight
+        , top_left: req.body.top_left
+        , middle_right: req.body.middle_right
+        , middle_straight: req.body.middle_straight
+        , middle_left: req.body.middle_left
+        , under_right: req.body.under_right
+        , under_straight: req.body.under_straight
+        , under_left: req.body.under_left
+    , });
+    goal.save(function (err) {
+        if (err) {
             console.log("保存できてないよ！");
-        }else{
+        } else {
             console.log("保存できました！")
         }
     });
     res.redirect("/goals")
 })
 
-app.get("/goals",function(req,res){
-    goallists =[];
-    Goal.find({},function(err,goals){
-        if(err) throw err
-        for (var i = 0; i<goals.length; i++){
-            goallists.push(goals[i]);
-        }
-        res.render(main_filename,{
-            name:"岡野",
-            goallists:goallists
-    });
-    })
-});
-
-app.delete("/goals/:id",function(req,res){
-    Goal.findById(req.params.id,function(err,goal){
+app.delete("/goals/:id", function (req, res) {
+    Goal.findById(req.params.id, function (err, goal) {
         goal.remove();
+        res.redirect("/goals")
     });
-    res.redirect("/goals")
+
 });
 
+app.get("/subscribe", function (req, res) {
+    res.render(subscribe_filename);
+})
+
+app.post("/create", function (req, res) {
+    var user = new User({
+        username: req.body.username
+        , password: req.body.password
+    })
+    user.save(function (err) {
+        if (err) throw err;
+        res.redirect("/goals");
+    })
+})
+
+//サーバー接続
 app.listen(3000, function () {
     console.log("接続完了！");
 });
